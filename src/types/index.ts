b570c31774868b9710d46114889a6cc26fa405d2
AndStrel <xoz9ih@outlook.com>
2024-08-02 22:50:06 +0300

    Начал написание документации (базовые типы данных ,слой данных)

---
+++ src/types/index.ts
@@ -0,0 +1,36 @@
+export interface ICard {
+    id: string;
+    description: string;
+    image: string;
+    tittle: string;
+    category: string;
+    price: number | null;
+    
+}
+
+export interface IUserData {
+    methodPay: string;
+    adress: string;
+    email: string;
+    phone: string;
+}
+
+export interface ICardsData {
+    cards: ICard[];
+    preview: string | null;
+    getCard(id: string): ICard;
+}
+
+export interface IBasket {
+    cardsBasket: ICard[] | null;
+    totalPrice: number;
+    counter: number;
+    addCard(card: ICard): void;
+    deleteCard(id: string): void;
+}
+
+export type TCardCatalog = Pick<ICard, "id" |  "image" | "tittle" | "category" | "price">;
+export type TCardBasket = Pick<ICard, "tittle" | "price">;
+export type TUserDataOneStep = Pick<IUserData, "methodPay" | "adress">;
+export type TUserDataTwoStep = Pick<IUserData, "email" | "phone">;
+
